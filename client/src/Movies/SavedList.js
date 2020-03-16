77ed75fb9ac1a7d965c5169f9212255ca61564c8
mirage <mirage.kamran.pkt@gmail.com>
2020-03-15 22:44:29 -0700

    color styles added

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SavedList = props => (
-  <div className="saved-list">
+  <div className="saved-list" style={{backgroundColor:'#ced9eb'}}>
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <span className="saved-movie">{movie.title}</span>
